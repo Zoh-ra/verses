51ff1ebde70fd27bd01e228d0abcef91f6864cf0
zoh-ra <raitonoums@gmail.com>
2024-11-10 19:10:46 +0100

    first commit

---
+++ src/main/java/com/coranapp/verses/VersesApplication.java
@@ -0,0 +1,13 @@
+package com.coranapp.verses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VersesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VersesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coranapp/verses/VersesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coranapp.verses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VersesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
